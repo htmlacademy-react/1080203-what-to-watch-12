63ec3cd1403fbd14f09765f55327fa639a70f4be
Eujenio Gonzalez <vegas-host@yandex.ru>
2023-03-04 11:38:54 +0300

    Created pages components

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,8 +6,18 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const FilmInfo = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  release: 2014
+} as const;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      title = {FilmInfo.title}
+      genre = {FilmInfo.genre}
+      release = {FilmInfo.release}
+    />
   </React.StrictMode>,
 );
